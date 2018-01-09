fac23abc3ca3abe627df5753e8f9f17556660c7f
wangxiangqian022 <1296703504@qq.com>
2018-01-09 17:26:42 +0800

    loading

--- src/pages/list-detail/list-detail.ts
+++ src/pages/list-detail/list-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HttpServiceProvider} from '../../providers/http-service/http-service';
+import { LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the ListDetailPage page.
@@ -18,7 +19,10 @@ export class ListDetailPage {
   public  listDetial= [];
   public  data=[];
   public todo ={};
-  constructor(public navCtrl: NavController, public navParams: NavParams, public service:HttpServiceProvider) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public service:HttpServiceProvider,
+              public loadingCtrl: LoadingController) {
     this.listDetial = this.navParams.data.item ;
     console.log(JSON.stringify(this.listDetial))
   }
@@ -28,14 +32,20 @@ export class ListDetailPage {
     this.loadDetail();
   }
   loadDetail(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
     this.service.list('/system/funcdef/detail/T_SAL_OUTSTOCK/' + this.listDetial["OUTSTOCK_FOREIGNKEY"],'').then(data=>{
+      loader.dismiss();
       if(data.length>0){
         this.data = data;
       }
     });
+
   }
   logForm(){
-    debugger;
+
     alert('1');
   }
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import{ ListDetailPage} from '../list-detail/list-detail';
 import {HttpServiceProvider} from '../../providers/http-service/http-service';
+import { LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the ListPage page.
@@ -17,7 +18,10 @@ import {HttpServiceProvider} from '../../providers/http-service/http-service';
 })
 export class ListPage {
   public  items = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams,public service:HttpServiceProvider) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public service:HttpServiceProvider,
+              public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
@@ -28,10 +32,23 @@ export class ListPage {
    this.navCtrl.push(ListDetailPage, { item: item });
   }
   loadDate(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
     this.service.list('/system/funcdef/T_SAL_OUTSTOCK/query',{}).then(data=>{
+      loader.dismiss();
       if( data.data.records.length > 0 ){
         this.items = data.data.records;
       }
     });
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
   }
 }
