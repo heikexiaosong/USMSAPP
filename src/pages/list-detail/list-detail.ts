ff585611ea1bb159695c5fde3b5cd520f266303c
wangxiangqian022 <1296703504@qq.com>
2018-01-09 18:29:48 +0800

    loading

--- src/pages/list-detail/list-detail.ts
+++ src/pages/list-detail/list-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HttpServiceProvider} from '../../providers/http-service/http-service';
 import {ListDetailInputPage} from '../list-detail-input/list-detail-input';
 import { ModalController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the ListDetailPage page.
@@ -23,7 +24,8 @@ export class ListDetailPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public service:HttpServiceProvider,
-              public modalCtrl: ModalController) {
+              public modalCtrl: ModalController,
+              public loadingCtrl: LoadingController) {
     this.listDetial = this.navParams.data.item ;
     console.log(JSON.stringify(this.listDetial));
   }
@@ -33,7 +35,12 @@ export class ListDetailPage {
     this.loadDetail();
   }
   loadDetail(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
     this.service.list('/system/funcdef/detail/T_SAL_OUTSTOCK/' + this.listDetial["OUTSTOCK_FOREIGNKEY"],'').then(data=>{
+      loader.dismiss();
       const record = this.master;
       data.data.master.forEach(
         function (eachObj) {
